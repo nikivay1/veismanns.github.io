6a6bc5fe4f067982416c663f73c00f32f59cec19
Nikita <nikivay312@gmail.com>
2026-07-22 15:52:57 +0200

    Add deploy on Github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nikivay1.github.io',
+  base: '/veismanns.github.io',
   i18n: {
     locales: ['en', 'ru', 'es'],
     defaultLocale: 'en',
